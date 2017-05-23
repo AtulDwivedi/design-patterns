14a5663753d2d47c102c5f27ace5b912cbc3dd69
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-05-23 21:08:50 +0530

    Added singleton design pattern implementation

---
+++ src/com/atuldwivedi/dp/creational/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package com.atuldwivedi.dp.creational.singleton;
+
+public class Singleton {
+
+	private static Singleton instance;
+
+	private Singleton() {
+		// ...
+	}
+
+	public static synchronized Singleton getInstance() {
+		if (instance == null)
+			instance = new Singleton();
+
+		return instance;
+	}
+
+	// ...
+	public void doSomething() {
+		// ...
+	}
+}
